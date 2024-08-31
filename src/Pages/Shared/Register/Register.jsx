c1d5e08b7b2fa2284ae602896c78e94319995535
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-31 18:46:25 +0600

    register page working

--- src/Pages/Shared/Register/Register.jsx
+++ src/Pages/Shared/Register/Register.jsx
@@ -1,10 +1,103 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Register = () => {
+const Register = () => { 
+const handleRegister = (e)=>{
+     e.preventDefault()
+   
+     const name = e.target.name.value
+     const image = e.target.image.value
+     const email = e.target.email.value
+     const password = e.target.password.value
+     console.log(name,image,email,password);
+     
+}
      return (
           <div>
-               
+          <div className="bg-[#141414] rounded-lg py-2">
+               <div className="container flex flex-col mx-auto   rounded-lg  my-5">
+                    <div >
+                         <div className=" max-w-xl mx-auto pb-12">
+                              <div className="flex items-center ">
+                                   <form onSubmit={handleRegister} className="flex flex-col w-full h-full pb-6 text-center bg-[#191919] p-12  rounded-3xl">
+                                        <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Register </h3>
+                                        <p className="mb-4 text-grey-700">Enter your email and password</p>
+                                        <button className="flex items-center justify-center w-full py-4 mb-6 border-2 border-[#703bf7] text-white text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
+                                             <img
+                                                  className="h-5 mr-2"
+                                                  src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
+                                                  alt=""
+                                             />
+                                             Sign in with Google
+                                        </button>
+                                        <div className="flex items-center mb-3">
+                                             <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                                             <p className="mx-4 text-grey-600">or</p>
+                                             <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                                        </div>
+                                        <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">
+                                             Name
+                                        </label>
+                                        <input
+                                             id="name"
+                                             type="text"
+                                             name='name'
+                                             placeholder="Type your name please"
+                                             className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                        />
+                                        <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">
+                                             image
+                                        </label>
+                                        <input
+                                             id="image"
+                                             type="text"
+                                             name='image'
+                                             placeholder="Enter Your Image"
+                                             className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                        />
+                                        <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">
+                                             Email*
+                                        </label>
+                                        <input
+                                             id="email"
+                                             type="email"
+                                             name='email'
+                                             placeholder="mail@loopple.com"
+                                             className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                        />
+                                        <label htmlFor="password" className="mb-2 text-sm text-start text-grey-900">
+                                             Password*
+                                        </label>
+                                        <input
+                                             id="password"
+                                             type="password"
+                                             name='password'
+                                             placeholder="Enter a password"
+                                             className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                        />
+
+                                        <button
+                                        type='submit'
+                                             
+                                             className="relative mb-4 inline-block px-7 py-3 text-lg font-bold text-white border-2 border-[#703bf7] overflow-hidden bg-transparent transition-all duration-300 ease-in-out hover:bg-[#703bf7]"
+                                        >
+                                             <span className="relative z-10"> Register </span>
+                                             
+                                        </button>
+                                        <p className="text-sm leading-relaxed text-grey-900">
+                                            Already Have an Account?{" "}
+                                             <NavLink to={'/login'} className="font-bold text-grey-700">
+                                                  Login Here
+                                             </NavLink>
+                                        </p>
+                                   </form>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
           </div>
+     </div>
      );
 };
 
